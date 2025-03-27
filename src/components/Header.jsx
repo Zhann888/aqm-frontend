b6b934ab6df55103f6fdbff3eaec1af3374d7e0c
Aruzhan <ru.baur48@gmail.com>
2025-03-28 01:19:22 +0500

    адаптивность

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "../index.css";
 
-const Header = ({ onOpenModal }) => (
-  <header>
-    <div className="logo">Logo Name</div>
+const Header = ({ onOpenModal }) => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-    <div className="nav-actions">
-      <nav>
-        <ul>
-          <li><a href="#home">Главная</a></li>
-          <li><a href="#info">Справочник</a></li>
-          <li><a href="#about">О проекте</a></li>
-          <li><a href="#contacts">Контакты</a></li>
-        </ul>
-      </nav>
+  const toggleMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
 
-      <button onClick={onOpenModal} className="complaint-button">
-        Оставить жалобу
+  return (
+    <header>
+      <div className="logo">Logo Name</div>
+
+      <button className="burger" onClick={toggleMenu}>
+        ☰
       </button>
-    </div>
-  </header>
-);
+
+      <div className={`nav-actions ${isMobileMenuOpen ? "open" : ""}`}>
+        <nav>
+          <ul>
+            <li><a href="#home">Главная</a></li>
+            <li><a href="#info">Справочник</a></li>
+            <li><a href="#about">О проекте</a></li>
+            <li><a href="#contacts">Контакты</a></li>
+          </ul>
+        </nav>
+
+        <button className="complaint-button" onClick={onOpenModal}>
+          Оставить жалобу
+        </button>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
